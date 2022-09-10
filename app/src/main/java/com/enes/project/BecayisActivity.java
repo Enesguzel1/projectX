687b3c394374ea86883768124d4a3204df5b623b
Umut Saydam <umutsaydam24@gmail.com>
2022-09-10 17:32:17 +0300

    last commit

--- app/src/main/java/com/enes/project/BecayisActivity.java
+++ app/src/main/java/com/enes/project/BecayisActivity.java
@@ -10,5 +10,6 @@ public class BecayisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_becayis);
+        // last commit
     }
 }
