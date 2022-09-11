a489858b4f498c4630b6a6cbc9e3750d769adca5
Enesguzel1 <enesguzel4601@outlook.com>
2022-09-12 00:31:25 +0300

    tasarım bitti

--- app/src/main/java/com/enes/project/HomePageActivity.java
+++ app/src/main/java/com/enes/project/HomePageActivity.java
@@ -8,35 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
-    private Button becayisButton,alSatButton,soruCevapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        becayisButton = findViewById(R.id.becayisButton);
-        alSatButton = findViewById(R.id.alSatButton);
-        soruCevapButton = findViewById(R.id.soruCevapButton);
 
-        //aktivitelere yönlendirme
-        becayisButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToBecayisActivity();
-            }
-        });
-        alSatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToAlSatActivity();
-            }
-        });
-        soruCevapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToSoruCevapActivity();
-            }
-        });
 
 
 
