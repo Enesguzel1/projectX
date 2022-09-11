122f1416e494ae2b4c704cc87fb45c5e74d99794
Umut Saydam <umutsaydam24@gmail.com>
2022-09-11 15:53:14 +0300

    added menu in al_sat_home_page with Toast Message

--- app/src/main/java/com/enes/project/AlSatActivity.java
+++ app/src/main/java/com/enes/project/AlSatActivity.java
@@ -1,14 +1,33 @@
 package com.enes.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class AlSatActivity extends AppCompatActivity {
+    private BottomNavigationView alSatNavBottomMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_al_sat);
+
+        alSatNavBottomMenu = findViewById(R.id.alSatNavBottomMenu);
+
+        alSatNavBottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(AlSatActivity.this, ""+item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
+
 }
