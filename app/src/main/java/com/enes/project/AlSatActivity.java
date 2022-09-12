d4f8e2ccecdb3af262c1f85203084d620067dfa5
Umut Saydam <umutsaydam24@gmail.com>
2022-09-12 09:34:57 +0300

    some fragments directed

--- app/src/main/java/com/enes/project/AlSatActivity.java
+++ app/src/main/java/com/enes/project/AlSatActivity.java
@@ -2,12 +2,21 @@ package com.enes.project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Toast;
 
+import com.enes.project.Fragments.AlSatHomePageFragment;
+import com.enes.project.Fragments.myAdvertiseFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -24,24 +33,41 @@ public class AlSatActivity extends AppCompatActivity {
         alSatNavBottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.menu_al_sat_home:
-                        Toast.makeText(AlSatActivity.this, "HOME", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.menu_al_sat_advertise:
-                        Toast.makeText(AlSatActivity.this, "Adversite", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.menu_al_sat_my_advertise:
-                        Toast.makeText(AlSatActivity.this, "My Adversite", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.menu_al_sat_my_profile:
-                        Toast.makeText(AlSatActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                        break;
-
+                int itemId = item.getItemId();
+                if (itemId == R.id.menu_al_sat_home) {
+                    Toast.makeText(AlSatActivity.this, "HOME", Toast.LENGTH_SHORT).show();
+                    setFragment(new AlSatHomePageFragment());
+                } else if (itemId == R.id.menu_al_sat_advertise) {
+                    bottomSheetDialog();
+                    Toast.makeText(AlSatActivity.this, "Adversite", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.menu_al_sat_my_advertise) {
+                    Toast.makeText(AlSatActivity.this, "My Adversite", Toast.LENGTH_SHORT).show();
+                    setFragment(new myAdvertiseFragment());
+                } else if (itemId == R.id.menu_al_sat_my_profile) {
+                    Toast.makeText(AlSatActivity.this, "Profile", Toast.LENGTH_SHORT).show();
                 }
                 return true;
             }
         });
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView2, fragment);
+        transaction.commit();
+    }
+
+    private void bottomSheetDialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.al_sat_bottom_sheet_modal);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations =R.style.DialoAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
 }
