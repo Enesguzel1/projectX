b9146920d0c0f1ef1875db0edb44404d06e807d2
Enesguzel1 <enesguzel4601@outlook.com>
2022-09-13 23:13:21 +0300

    IDs of cardView are added and directions are made

--- app/src/main/java/com/enes/project/HomePageActivity.java
+++ app/src/main/java/com/enes/project/HomePageActivity.java
@@ -1,6 +1,7 @@
 package com.enes.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,14 +9,40 @@ import android.view.View;
 import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
+    CardView cardViewBecayis,cardViewAlSat,cardViewSoruCevap,cardViewOneri,cardViewYardım,cardViewAyarlar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        cardViewBecayis=findViewById(R.id.cardViewBecayis);
+        cardViewAlSat=findViewById(R.id.cardViewAlSat);
+        cardViewSoruCevap=findViewById(R.id.cardViewSoruCevap);
+        cardViewOneri=findViewById(R.id.cardViewOneri);
+        cardViewYardım=findViewById(R.id.cardViewYardım);
+        cardViewAyarlar=findViewById(R.id.cardViewAyarlar);
+        //yönlendirmeler
+        cardViewBecayis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToBecayisActivity();
+            }
+        });
 
+        cardViewAlSat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToAlSatActivity();
+            }
+        });
 
+        cardViewSoruCevap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSoruCevapActivity();
+            }
+        });
 
     }
 
