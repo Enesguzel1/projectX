6323a8c79f07e1e8ffe9e84e24b6470a2f84fdda
Umut Saydam <umutsaydam24@gmail.com>
2022-09-14 20:03:43 +0300

    renamed

--- app/src/main/java/com/enes/project/Fragments/alSatMyAdvertiseFragment.java
+++ app/src/main/java/com/enes/project/Fragments/alSatMyAdvertiseFragment.java
@@ -10,24 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.enes.project.Adapters.alSatAdapter;
+import com.enes.project.Adapters.AlSatMyAdvertiseAdapter;
 import com.enes.project.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class myAdvertiseFragment extends Fragment {
+public class alSatMyAdvertiseFragment extends Fragment {
     private ViewPager2 viewPager;
     private TabLayout tabLayout;
-    private alSatAdapter adapter;
+    private AlSatMyAdvertiseAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_advertise, container, false);
+        View view = inflater.inflate(R.layout.fragment_al_sat_my_advertise, container, false);
 
         viewPager = view.findViewById(R.id.alSatViewPager);
 
-        adapter = new alSatAdapter(getParentFragmentManager(), getLifecycle());
+        adapter = new AlSatMyAdvertiseAdapter(getParentFragmentManager(), getLifecycle());
         adapter.addFragment(new alSatMyAdvertiseForSaleFragment(), "İlandakiler");
         adapter.addFragment(new alSatMyAdvertiseSoldFragment(), "Satılanlar");
         viewPager.setAdapter(adapter);
