123b5c1398a53f7562692297e4602d69daeb9d1c
Umut Saydam <umutsaydam24@gmail.com>
2022-09-14 20:03:11 +0300

    setup Adapter

--- app/src/main/java/com/enes/project/Fragments/alSatMyAdvertiseSoldFragment.java
+++ app/src/main/java/com/enes/project/Fragments/alSatMyAdvertiseSoldFragment.java
@@ -3,64 +3,39 @@ package com.enes.project.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.enes.project.Adapters.AlSatAdvertiseAdapter;
+import com.enes.project.Models.AlSatAdvertiseModel;
 import com.enes.project.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link alSatMyAdvertiseSoldFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class alSatMyAdvertiseSoldFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public alSatMyAdvertiseSoldFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment alSatMyAdvertiseSoldFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static alSatMyAdvertiseSoldFragment newInstance(String param1, String param2) {
-        alSatMyAdvertiseSoldFragment fragment = new alSatMyAdvertiseSoldFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView recyclerView;
+    private AlSatAdvertiseAdapter adapter;
+    private ArrayList<AlSatAdvertiseModel> alSatAdvertiseModels;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_al_sat_my_advertise_sold, container, false);
+        View view = inflater.inflate(R.layout.fragment_al_sat_my_advertise_sold, container, false);
+
+        recyclerView = view.findViewById(R.id.alSatMyAdvertiseSold);
+
+        alSatAdvertiseModels = new ArrayList<>();
+        alSatAdvertiseModels.add(new AlSatAdvertiseModel("Title-1"));
+        alSatAdvertiseModels.add(new AlSatAdvertiseModel("Title-2"));
+        alSatAdvertiseModels.add(new AlSatAdvertiseModel("Title-3"));
+        alSatAdvertiseModels.add(new AlSatAdvertiseModel("Title-4"));
+
+        adapter = new AlSatAdvertiseAdapter(alSatAdvertiseModels, getContext());
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
