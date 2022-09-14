34d496b0f3f3d984cc92b1599db6e34875075cc9
Umut Saydam <umutsaydam24@gmail.com>
2022-09-14 20:02:10 +0300

    added Advertise model

---
+++ app/src/main/java/com/enes/project/Models/AlSatAdvertiseModel.java
@@ -0,0 +1,20 @@
+package com.enes.project.Models;
+
+public class AlSatAdvertiseModel {
+    String advertiseTitle;
+
+    public AlSatAdvertiseModel() {
+    }
+
+    public AlSatAdvertiseModel(String advertiseTitle) {
+        this.advertiseTitle = advertiseTitle;
+    }
+
+    public String getAdvertiseTitle() {
+        return advertiseTitle;
+    }
+
+    public void setAdvertiseTitle(String advertiseTitle) {
+        this.advertiseTitle = advertiseTitle;
+    }
+}
