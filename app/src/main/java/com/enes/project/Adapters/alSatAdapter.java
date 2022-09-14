f70adde125b580787a27ebf6c5cde9f7600ddf6c
Umut Saydam <umutsaydam24@gmail.com>
2022-09-14 12:17:40 +0300

    created adapter for ViewPager2

---
+++ app/src/main/java/com/enes/project/Adapters/alSatAdapter.java
@@ -0,0 +1,41 @@
+package com.enes.project.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class alSatAdapter extends FragmentStateAdapter {
+    ArrayList<Fragment> fragments;
+    ArrayList<String> pageTitles;
+
+    public alSatAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+        fragments = new ArrayList<>();
+        pageTitles = new ArrayList<>();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragments.add(fragment);
+        pageTitles.add(title);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+
+    public CharSequence getPageTitle(int position){
+        return pageTitles.get(position);
+    }
+
+}
